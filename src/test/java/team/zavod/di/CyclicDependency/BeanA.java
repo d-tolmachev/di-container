471bbd16eae6f8188add4943126609f88d5ed9d1
Mark Bukhner <markbuxner@mail.ru>
2023-03-29 06:14:56 +0700

    DI-0. Solved CyclidDependencies

---
+++ src/test/java/team/zavod/di/CyclicDependency/BeanA.java
@@ -0,0 +1,16 @@
+package team.zavod.di.CyclicDependency;
+
+import jakarta.inject.Inject;
+import team.zavod.di.annotation.Component;
+import team.zavod.di.annotation.Lazy;
+
+@Component
+@Lazy
+public class BeanA {
+  @Inject
+  private BeanB beanB;
+
+  public BeanB getBeanB() {
+    return this.beanB;
+  }
+}
---
+++ src/test/java/team/zavod/di/CyclicDependency/TestCyclicDependency.java
@@ -0,0 +1,19 @@
+package team.zavod.di.CyclicDependency;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import team.zavod.di.factory.BeanFactory;
+import team.zavod.di.factory.DefaultBeanFactory;
+
+public class TestCyclicDependency {
+  private final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+  private final BeanFactory beanFactory = new DefaultBeanFactory(new String[]{"team.zavod.di.CyclicDependency"}, this.classLoader);
+
+//  @Test
+//  void test() {
+//    BeanA beanA = beanFactory.getBean(BeanA.class);
+//    BeanB beanB = beanFactory.getBean(BeanB.class);
+//    Assertions.assertSame(beanA, beanB.getBeanA());
+//    Assertions.assertSame(beanB, beanA.getBeanB());
+//  }
+}
