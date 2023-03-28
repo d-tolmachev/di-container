054286c243933bb8a9152ecc9ebc7e4f354a8707
dtolmachev1 <d.tolmachev1@g.nsu.ru>
2023-03-29 02:31:20 +0700

    DI-3. Primary

--- src/main/java/team/zavod/di/configuration/GenericBeanConfigurator.java
+++ src/main/java/team/zavod/di/configuration/GenericBeanConfigurator.java
@@ -1,6 +1,7 @@
 package team.zavod.di.configuration;
 
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
@@ -27,13 +28,17 @@ public class GenericBeanConfigurator implements BeanConfigurator {
   @Override
   public <T> Class<? extends T> getImplementationClass(Class<T> requiredType) throws NoSuchBeanException {
     if (!this.interfacesToImplementations.containsKey(requiredType)) {
-      Set<Class<? extends T>> implementationClasses = this.classpathHelper.getSubTypesOf(requiredType).stream()
+      Set<Class<? extends T>> subTypes = this.classpathHelper.getSubTypesOf(requiredType).stream()
           .filter(subType -> this.beanDefinitionRegistry.containsBeanDefinition(subType.getName()))
           .collect(Collectors.toSet());
-      if (implementationClasses.size() > 1) {
+      Class<? extends T> implementationClass = subTypes.stream().filter(subType -> this.beanDefinitionRegistry.getBeanDefinition(subType.getName()).isPrimary()).findFirst().orElse(null);
+      if (Objects.isNull(implementationClass) && subTypes.size() > 1) {
         throw new NoUniqueBeanException("Error! " + requiredType.getName() + " has more than one implementations!");
-      } else if (!implementationClasses.isEmpty()) {
-        this.interfacesToImplementations.put(requiredType, implementationClasses.iterator().next());
+      } else if (!subTypes.isEmpty()) {
+        if (Objects.isNull(implementationClass)) {
+          implementationClass = subTypes.iterator().next();
+        }
+        this.interfacesToImplementations.put(requiredType, implementationClass);
       } else throw new NoSuchBeanException("Error! " + requiredType.getName() + " has no implementations!");
     }
     return (Class<? extends T>) this.interfacesToImplementations.get(requiredType);
